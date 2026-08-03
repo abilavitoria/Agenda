3550c77cc01e4a5d000d769130479d9b6ca0381a
abilavitoria <toriaraujo6@gmail.com>
2026-08-02 21:36:15 -0300

    feat: iniciei a API

--- agenda/src/main/java/com/octupus/agenda/model/Evento.java
+++ agenda/src/main/java/com/octupus/agenda/model/Evento.java
@@ -1,4 +1,22 @@
 package com.octupus.agenda.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
 public class Evento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Number id;
+    @Column
+    private String  titulo;
+    @Column
+    private Date dataEvento;
+    @Column
+    private Date dataLembrete;
+    @Column
+    private String categoria;
 }
