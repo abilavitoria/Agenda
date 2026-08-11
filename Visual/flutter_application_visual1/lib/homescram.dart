1e9bce2a204295df692631be12b1360fcfbacc89
abilavitoria <toriaraujo6@gmail.com>
2026-08-11 16:19:09 -0300

    feat: iniciei o visual

---
+++ Visual/flutter_application_visual1/lib/homescram.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class HomeScream extends StatelessWidget {
+  const HomeScream({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFF7F5F0),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          'Minha Agenda',
+          style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
+        ),
+
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 12.0),
+            child: TextButton.icon(
+              onPressed: (){
+
+              },
+              icon: const Icon(Icons.calendar_month, color: Colors.deepPurple,),
+              label: const Text(
+                'Calendário',
+                style: TextStyle( color: Colors.deepPurple, fontWeight: FontWeight.bold),
+              ),
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.deepPurple.shade50,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                )
+              ),
+
+              ),
+            )
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 20,),
+
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              child: Text(
+                'Toque no botão abaixo para ditar seu evento',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18, color: Colors.black54),
+              ),
+              ),
+            const Spacer(),
+
+            Center(
+              child: GestureDetector(
+                onTap: () => _exibirModalGravacao(context),
+              ),
+            ),
+
+            const Spacer(),
+
+            Container(
+              margin: const EdgeInsets.all(20),
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.05),
+                    blurRadius: 10,
+                    offset: const Offset(0, 4),
+                    ),
+                ]
+              ),
+
+              child: const Row(
+                children: [
+                  Icon(Icons.event_note, color: Colors.deepPurple, size: 30,),
+                  SizedBox(width: 12),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Próximo evento:',
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                      Text(
+                        'Casamento - 20/04/2027',
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                      )
+                    ],
+                  )
+                ],
+                ),
+            )
+          ],
+        ) 
+        ),
+    );
+  }
+
+  void _exibirModalGravacao(BuildContext context) {}
+}
