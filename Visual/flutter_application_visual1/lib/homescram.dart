a9835317fe4c2bad011079c8feae96829d1ea5a8
abilavitoria <toriaraujo6@gmail.com>
2026-08-13 10:09:49 -0300

    feat: correçoes no visual

--- Visual/flutter_application_visual1/lib/homescram.dart
+++ Visual/flutter_application_visual1/lib/homescram.dart
@@ -1,34 +1,51 @@
 import 'package:flutter/material.dart';
 
 class HomeScream extends StatelessWidget {
-  const HomeScream({super.key});
+  final VoidCallback onToogleTheme;
+  final bool isDarkMode;
+
+  const HomeScream({
+    super.key,
+    required this.onToogleTheme,
+    required this.isDarkMode,
+    });
 
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+
     return Scaffold(
-      backgroundColor: const Color(0xFFF7F5F0),
+      backgroundColor: theme.scaffoldBackgroundColor,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: const Text(
+        title: Text(
           'Minha Agenda',
-          style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold),
+          style: TextStyle(
+            color: colorScheme.onSurface,
+            fontWeight: FontWeight.bold),
         ),
 
         actions: [
+          IconButton(
+            onPressed: onToogleTheme,
+            icon: Icon(
+              isDarkMode ? Icons.light_mode : Icons.dark_mode,
+              color: isDarkMode ? const Color(0xFFFFD166) : colorScheme.primary,
+            ),),
+
           Padding(
             padding: const EdgeInsets.only(right: 12.0),
             child: TextButton.icon(
-              onPressed: (){
-
-              },
-              icon: const Icon(Icons.calendar_month, color: Colors.deepPurple,),
-              label: const Text(
+              onPressed: (){},
+              icon: Icon(Icons.calendar_month, color: colorScheme.primary,),
+              label: Text(
                 'Calendário',
-                style: TextStyle( color: Colors.deepPurple, fontWeight: FontWeight.bold),
+                style: TextStyle( color: colorScheme.primary, fontWeight: FontWeight.bold),
               ),
               style: TextButton.styleFrom(
-                backgroundColor: Colors.deepPurple.shade50,
+                backgroundColor: colorScheme.primary.withOpacity(0.15),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20),
                 )
@@ -41,14 +58,15 @@ class HomeScream extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            const SizedBox(height: 20,),
+            const SizedBox(height: 20),
 
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 24.0),
+           Padding( 
+              padding: const EdgeInsets.symmetric(horizontal: 24.0),
               child: Text(
                 'Toque no botão abaixo para ditar seu evento',
                 textAlign: TextAlign.center,
-                style: TextStyle(fontSize: 18, color: Colors.black54),
+                style: TextStyle(fontSize: 18, color: colorScheme.onSurface.withOpacity(0.7),
+                ),
               ),
               ),
             const Spacer(),
@@ -56,6 +74,30 @@ class HomeScream extends StatelessWidget {
             Center(
               child: GestureContainer(
                 onTap: () => _exibirModalGravacao(context),
+                child: AnimatedContainer(
+                  duration: const Duration(milliseconds: 300),
+                  width: 170,
+                  height: 170,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: colorScheme.primary,
+                    boxShadow: [
+                      BoxShadow(
+                        color: isDarkMode
+                          ? colorScheme.primary.withOpacity(0.6)
+                          : colorScheme.primary.withOpacity(0.35),
+                        blurRadius: isDarkMode ? 35 : 20,
+                        spreadRadius: isDarkMode ? 8 : 4,                
+                      )
+                    ]
+                  ),
+
+                  child: const Icon(
+                    Icons.mic,
+                    size: 85,
+                    color: Colors.white,
+                  ),
+                  )
               ),
             ),
 
@@ -65,31 +107,34 @@ class HomeScream extends StatelessWidget {
               margin: const EdgeInsets.all(20),
               padding: const EdgeInsets.all(16),
               decoration: BoxDecoration(
-                color: Colors.white,
+                color: colorScheme.onSurface,
                 borderRadius: BorderRadius.circular(16),
+                border: isDarkMode ? Border.all(color: colorScheme.primary.withOpacity(0.3)) : null,
                 boxShadow: [
                   BoxShadow(
-                    color: Colors.black.withOpacity(0.05),
+                    color: Colors.black.withOpacity(isDarkMode ? 0.4 : 0.08),
                     blurRadius: 10,
                     offset: const Offset(0, 4),
                     ),
                 ]
               ),
 
-              child: const Row(
+              child: Row(
                 children: [
-                  Icon(Icons.event_note, color: Colors.deepPurple, size: 30,),
+                  Icon(Icons.event_note, color: colorScheme.primary, size: 30),
                   SizedBox(width: 12),
-                  Column(
+                  Column( 
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         'Próximo evento:',
-                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                        style: TextStyle(fontSize: 12, color: colorScheme.onSurface.withOpacity(0.6)
+                        ),
                       ),
+                      const SizedBox(height: 2,),
                       Text(
                         'Casamento - 20/04/2027',
-                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                        style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold, color: colorScheme.onSurface),
                       )
                     ],
                   )
@@ -103,45 +148,50 @@ class HomeScream extends StatelessWidget {
   }
 
   void _exibirModalGravacao(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
     showModalBottomSheet(
       context: context,
       isScrollControlled: true,
+      backgroundColor: colorScheme.surface,
       shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+        borderRadius: BorderRadius.vertical(top: Radius.circular(28)),
       ),
       builder: (context){
         return Padding(
           padding: EdgeInsets.only(
-            top: 24,
+            top: 28,
             left: 24,
             right: 24,
-            bottom: MediaQuery.of(context).viewInsets.bottom + 24,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 28,
             ),
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
-                const Text(
+                Text(
                   'Ouvindo...',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold, color: colorScheme.onSurface),
                 ),
-                const SizedBox(height: 20,),
-                const Icon(Icons.graphic_eq, size: 60, color: Colors.deepPurple,),
-                const SizedBox(height: 20,),
-                const Text(
+                const SizedBox(height: 24),
+                Icon(Icons.graphic_eq, size: 70, color: colorScheme.primary),
+                const SizedBox(height: 20),
+                Text(
                   'Fale o título e a data do próximo evento. \nExemplo: "Casamento no dia 20 de abril de 2027 às 16h"',
                   textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.grey),
+                  style: TextStyle(color: colorScheme.onSurface.withOpacity(0.7),
+                  ),
                 ),
-                const SizedBox(height: 30,),
+                const SizedBox(height: 32),
                 ElevatedButton(
                   onPressed: (){
                     Navigator.pop(context);
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurple,
-                    minimumSize: const Size(double.infinity, 50),
+                    backgroundColor: colorScheme.primary,
+                    foregroundColor: Colors.white,
+                    minimumSize: const Size(double.infinity, 52),
                   ),
-                  child: const Text('Confirmar e salvar', style: TextStyle(color: Colors.white)),
+                  child: const Text('Confirmar e salvar', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
                   )
               ],
             ),
@@ -153,32 +203,14 @@ class HomeScream extends StatelessWidget {
 
 class GestureContainer extends StatelessWidget{
   final VoidCallback onTap;
-  const GestureContainer({super.key, required this.onTap});
+  final Widget child;
+  const GestureContainer({super.key, required this.onTap, required this.child});
 
+@override
   Widget build(BuildContext context){
     return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: 160,
-        height: 160,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.deepPurple,
-          boxShadow: [
-            BoxShadow(
-              color: Colors.deepPurple.withOpacity(0.3),
-              blurRadius: 25,
-              spreadRadius: 10,
-
-            )
-          ]
-        ),
-        child: const Icon(
-          Icons.mic,
-          size: 80,
-          color: Colors.white,
-        ),
-      ),
+      onTap: onTap, 
+      child: child
     );
   }
 }
--- Visual/flutter_application_visual1/lib/main.dart
+++ Visual/flutter_application_visual1/lib/main.dart
@@ -2,22 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_visual1/homescram.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AgendaApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AgendaApp extends StatefulWidget {
+  const AgendaApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Agenda em Audio',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: HomeScream(),
-    );
-  }
+  State<AgendaApp> createState() => _AgendaAppState();
 }
+
+class _AgendaAppState extends State<AgendaApp> {
+  ThemeMode _themeMode = ThemeMode.dark;
+
+  void _toogleTheme() {
+    setState(() {
+      _themeMode = _themeMode == ThemeMode.light
+          ? ThemeMode.dark
+          : ThemeMode.light;
+    });
+  }
+  @override
+Widget build(BuildContext context) {
+  final lightTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: const Color(0xFFF4F0EA),
+    colorScheme: const ColorScheme.light(
+      primary: Color(0xFF7209B7),
+      secondary: Color(0xFF3F37C9),
+      surface: Colors.white,
+      onSurface: Colors.black87,
+    ),
+  );
+
+  final darkTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: const Color(0xFF0D0B14),
+    colorScheme: const ColorScheme.dark(
+      primary: Color(0xFFD042FF),
+      secondary: Color(0xFF00F5D4),
+      surface: Color(0xFF181325),
+      onSurface: Colors.white,
+    ),
+  );
+
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Agenda Prática',
+    theme: lightTheme,
+    darkTheme: darkTheme,
+    themeMode: _themeMode,
+    home: HomeScream(
+      onToogleTheme: _toogleTheme,
+      isDarkMode: _themeMode == ThemeMode.dark,
+    ),
+  );
+}
+
+}
+
